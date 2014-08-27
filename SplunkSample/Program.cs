cb5b8b20a94d17ce3bfa88662ca2f1e38ab70903
Matthew Erbs <matthew.erbs@merbla.com>
2014-08-27 21:16:05 +1000

    updated to new 1.4.5 Beta Splunk Sink

--- SplunkSample/Program.cs
+++ SplunkSample/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Globalization;
 using System.Linq;
+using System.Net;
 using Serilog;
 using Serilog.Enrichers;
 using Serilog.Sinks.Splunk;
 using Splunk;
+using Splunk.Client;
 
 namespace SplunkSample
 {
@@ -11,32 +14,43 @@ namespace SplunkSample
     {
         static void Main()
         {
-            const string host = "192.168.93.128";
-            const string username = "serilog";
-            const string password = "serilog";
+            ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
 
-            var connectArgs = new ServiceArgs { Host = host, };
+            //Generally it is not advised to log to Splunk via HTTP/HTTPS.  This is available for mobile apps and other special cases.
+         
+            const string host = "127.0.0.1";
 
-            var splunkConnectionInfoInfo = new SplunkConnectionInfo
+            //Only used for HTTP/HTTPS scenarios
+            var otherContext = new Context(Scheme.Https, host, 8089);
+
+            var transmitterArgs = new TransmitterArgs
             {
-                ServiceArgs = connectArgs,
-                Username = username,
-                Password = password,
-                SplunkEventType = "Serilog",
-                SplunkSource = "Serilog.SplunkSample"
+                Source = "Splunk.Sample",
+                SourceType = "Splunk Sample Source"
             };
+            const string username = "my splunk user";
+            const string password = "my splunk password";
+            const string splunkIndex = "mysplunktest";
+
+            var splunkContext = new SplunkContext(otherContext, splunkIndex, username, password, null, transmitterArgs);
 
             Log.Logger = new LoggerConfiguration()
                 .Enrich.With(new ThreadIdEnricher())
+                .Enrich.WithMachineName()
                 .WriteTo.ColoredConsole()
-                .WriteTo.Splunk(splunkConnectionInfoInfo, 3, TimeSpan.FromSeconds(1))
+                .WriteTo.SplunkViaHttp(splunkContext, 10, TimeSpan.FromSeconds(5))
+
+                //See http://docs.splunk.com/Documentation/Splunk/6.1.3/Data/Monitornetworkports
+                .WriteTo.SplunkViaUdp(host, 10000)
+                .WriteTo.SplunkViaTcp(host, 10001)
+
                 .CreateLogger();
 
             var serilogLogger = Log.ForContext<Program>();
 
             serilogLogger.Information("Hello from Serilog, running as {Username}!", Environment.UserName);
 
-            var items = Enumerable.Range(1,10);
+            var items = Enumerable.Range(1, 1000);
 
             foreach (var item in items)
             {
